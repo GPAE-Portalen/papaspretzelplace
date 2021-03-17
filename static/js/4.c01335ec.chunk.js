78b57364355e2103fcaa9f0c385dbe1358381221
Gustav Eklund <gustavpaeklund@gmail.com>
2021-03-17 22:30:16 +0100

    Updates

--- static/js/4.c01335ec.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppapaspretzelplace.com"]=this["webpackJsonppapaspretzelplace.com"]||[]).push([[4],{39:function(e,t,c){"use strict";c.r(t),c.d(t,"default",(function(){return a}));var n=c(0),s=c.n(n),p=c(6);function a(){return Object(p.jsxs)(s.a.Fragment,{children:[Object(p.jsx)("h1",{children:"404"}),Object(p.jsx)("p",{children:"This page doesn't exist!"})]})}}}]);
-//# sourceMappingURL=4.c01335ec.chunk.js.map
