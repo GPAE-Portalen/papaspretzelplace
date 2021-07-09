0c0e1e9a584053232b41e50771a4b2f0511cb6d2
Gustav Eklund <gustavpaeklund@gmail.com>
2021-07-09 05:33:46 +0200

    Fix app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { BootstrapColor, bootstrapColorName } from '../enums/bootstrap';
-
-import Alert from './components/Alert';
-
 const Home = React.lazy(() => import("./pages/Home"));
 const Menu = React.lazy(() => import("./pages/Menu"));
-const Products = React.lazy(() => import("./pages/Products"));
-const Product = React.lazy(() => import("./pages/Product"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
-
-function AppOld() {
-    return (
-        <React.Fragment>
-            <Alert type={bootstrapColorName[BootstrapColor.Light]} className={`border-bottom color-${bootstrapColorName[BootstrapColor.PppRed]}`}>
-                <span>This website is under construction! Visit our <a href="https://www.facebook.com/papaspretzelplace" target="_blank" rel="noreferrer">Facebook page</a> for more information.</span>
-            </Alert>
-
-            <Router>
-                <React.Suspense fallback={<div className="d-none">Loading...</div>}>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/products" component={Products} />
-                        <Route path="/products/:productName" component={Product} />
-                        <Route path="*" component={NotFound} />
-                    </Switch>
-                </React.Suspense>
-            </Router>
-        </React.Fragment>
-    );
-}
 
 export default function App() {
     return (
