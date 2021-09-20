d66657c1c02a331794ff23ba522b1ce97c575774
Gustav Eklund <gustavpaeklund@gmail.com>
2021-09-20 21:19:31 +0200

    Modify

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -14,3 +14,18 @@ export interface IBlogPost {
     title: string;
     date: Date;
 }
+
+export interface IDictionary<T> {
+    [key: string]: T;
+}
+
+export interface IPrezelPost {
+    name: string;
+    prices: IPrice[];
+    image?: string; 
+}
+
+export interface IPrice {
+    ammount: number;
+    name: string;
+}
