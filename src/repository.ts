804ae15b6940d61f41da4a9469d07554a967ac7d
Gustav Eklund <gustavpaeklund@gmail.com>
2021-09-22 17:59:15 +0200

    Remove old code

--- src/repository.ts
+++ src/repository.ts
@@ -1,9 +1,7 @@
-import { ModelStateError } from './modelStateError';
-
-import { IMenu, IDictionary, IMenuItem, IProduct } from './interfaces/index';
+import { IMenu, IDictionary, IMenuItem } from './interfaces/index';
 
 export interface IRepository {
-    getProducts(): IProduct[];
+    getMenu(): IMenu;
 }
 
 export default class Repository implements IRepository {
@@ -14,11 +12,11 @@ export default class Repository implements IRepository {
     }
 
     getMenu(): IMenu {
-        const pretzels: IDictionary<IMenuItem> = require(`${this.url}/pretzels.json`);
-        const pretzelDogs: IDictionary<IMenuItem> = require(`${this.url}/pretzelDogs.json`);
-        const iceCreams: IDictionary<IMenuItem> = require(`${this.url}/iceCreams.json`);
-        const waterIce: IDictionary<IMenuItem> = require(`${this.url}/waterIce.json`);
-        const dips: IDictionary<IMenuItem> = require(`${this.url}/dips.json`);
+        const pretzels: IDictionary<IMenuItem> = require(`${this.url}pretzels.json`);
+        const pretzelDogs: IDictionary<IMenuItem> = require(`${this.url}pretzelDogs.json`);
+        const iceCreams: IDictionary<IMenuItem> = require(`${this.url}iceCreams.json`);
+        const waterIce: IDictionary<IMenuItem> = require(`${this.url}waterIce.json`);
+        const dips: IDictionary<IMenuItem> = require(`${this.url}dips.json`);
         const menu: IMenu = {
             pretzels,
             pretzelDogs,
@@ -28,45 +26,5 @@ export default class Repository implements IRepository {
         };
 
         return menu;
-    }
-
-    getProducts(): IProduct[] {
-        const data: IProduct[] = require(`${this.url}/products.json`);
-        return data;
-    }
-
-    async getBlogPost(fileName: string) {
-        const resp = await fetch(`${this.url}/blog/${fileName}.json`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-
-        });
-        return resp.json();
-    }
-
-    async createBlogPost(fileName: string) {
-        const resp = await fetch(`/.netlify/functions/blog?fileName=${fileName}`);
-        return resp.json();
-    }
-
-    async deleteBlogPost(fileName: string) {
-        const resp = await fetch(`/.netlify/functions/deleteBlogPost?fileName=${fileName}`);
-        return resp.json();
-    }
-
-    private handleResponse(response: Response) {
-        const contentType = response.headers.get('Content-Type');
-        const isJson = (contentType && contentType.indexOf('application/json') !== -1) || false;
-
-        if (response.ok && isJson) {
-            return response.json();
-        }
-        else if ((response.status === 400 || response.status === 422) && isJson) {
-            return response.json().then(x => Promise.reject(new ModelStateError(`${response.status}`, x)));
-        }
-
-        return Promise.reject(new Error(`${response.status} ${response.statusText}`));
     }
 }
